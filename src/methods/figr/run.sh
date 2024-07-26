b163932318aef117b54930791489d68951dc878e
matin <matin@M-As-MacBook-Pro.local>
2024-07-26 10:32:42 +0100

    bug fixed in figr

--- src/methods/figr/run.sh
+++ src/methods/figr/run.sh
@@ -1,2 +1,9 @@
 viash build src/methods/figr/config.vsh.yaml -p docker -o bin_figr
-bin_figr/figr --multiomics_rna resources_test/grn-benchmark/multiomics_r/rna.rds --multiomics_atac resources_test/grn-benchmark/multiomics_r/atac.rds --prediction bin_figr/prediction.csv --cell_topic resources_test/grn-benchmark/supp/cell_topic.csv
+bin_figr/figr 
+ --multiomics_rna resources_test/grn-benchmark/multiomics_r/rna.rds 
+ --multiomics_atac resources_test/grn-benchmark/multiomics_r/atac.rds 
+ --prediction bin_figr/prediction.csv 
+ --cell_topic resources_test/grn-benchmark/supp/cell_topic.csv
+
+
+
