a8da63f10f600b11505dc06a5018bc395db866e1
jalil <jalil.nourisa@gmail.com>
2024-09-10 17:24:25 +0200

    workflows updated

--- src/control_methods/baseline_corr/script.py
+++ src/control_methods/baseline_corr/script.py
@@ -6,14 +6,10 @@ import scanpy as sc
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
 
-
 ## VIASH START
 par = {
-
 }
-
 ## VIASH END
-
 def create_corr_net(X: np.ndarray, groups: np.ndarray):
     grns = []
     for group in tqdm(np.unique(groups), desc="Processing groups"):
@@ -22,10 +18,10 @@ def create_corr_net(X: np.ndarray, groups: np.ndarray):
         grn = np.dot(X_sub.T, X_sub) / X_sub.shape[0]
         grns.append(grn)
     return np.mean(grns, axis=0)
-
-
 print('Read data')
 multiomics_rna = ad.read_h5ad(par["multiomics_rna"])
+# print('subsetting: remove this')
+# multiomics_rna = multiomics_rna[:5000, :5000]
 gene_names = multiomics_rna.var_names.to_numpy()
 tf_all = np.loadtxt(par['tf_all'], dtype=str)
 groups = multiomics_rna.obs.cell_type
@@ -39,14 +35,15 @@ sc.pp.scale(multiomics_rna)
 print('Create corr net')
 net = create_corr_net(multiomics_rna.X, groups)
 net = pd.DataFrame(net, index=gene_names, columns=gene_names)
+
 if par['causal']:
-    net_corr = net[tf_all]
+    net = net[tf_all]
 else:
-    net_corr = net.sample(len(tf_all), axis=1)
-net_corr = net_corr.reset_index().melt(id_vars='index', var_name='source', value_name='weight')
-net_corr.rename(columns={'index': 'target'}, inplace=True)
+    net = net.sample(len(tf_all), axis=1, random_state=par['seed'])
+    
+net = net.reset_index().melt(id_vars='index', var_name='source', value_name='weight')
+net.rename(columns={'index': 'target'}, inplace=True)
 
 
 print('Output GRN')
-net_corr.to_csv(par['prediction'])
-
+net.to_csv(par['prediction'])
