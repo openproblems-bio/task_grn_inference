8d058b1e866849c8e2784365b745c1b7e711a3d5
AntoinePassemiers <antoine.passemiers@gmail.com>
2024-09-10 13:33:12 +0200

    Improve the speed of some single-omics methods

--- src/methods/single_omics/pidc/test.sh
+++ src/methods/single_omics/pidc/test.sh
@@ -1,1 +1,2 @@
-viash run src/methods/single_omics/pidc/config.novsh.yaml -- --multiomics_rna resources_test/grn-benchmark/multiomics_rna.h5ad --prediction output/pidc/prediction.csv
+#viash run src/methods/single_omics/pidc/config.vsh.yaml -- --multiomics_rna resources_test/grn-benchmark/multiomics_rna.h5ad --prediction output/pidc/prediction.csv
+viash run src/methods/single_omics/pidc/config.vsh.yaml -- --multiomics_rna resources/grn-benchmark/multiomics_rna.h5ad --prediction output/pidc/prediction.csv
