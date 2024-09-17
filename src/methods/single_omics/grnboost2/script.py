e7a9781f1e77339a879199abf5977d777cf27ece
Jalil Nourisa <jalil.nourisa@gmail.com>
2024-09-17 19:53:18 +0200

    scenic+ dockerfile needs update

--- src/methods/single_omics/grnboost2/script.py
+++ src/methods/single_omics/grnboost2/script.py
@@ -7,8 +7,6 @@ from arboreto.algo import grnboost2
 from distributed import Client, LocalCluster
 from tqdm import tqdm
 import subprocess 
-import scanpy as sc
-
 
 
 
@@ -28,10 +26,7 @@ def process_links(net, par):
   return net
 # Load scRNA-seq data
 adata_rna = anndata.read_h5ad(par['multiomics_rna'])
-print('Noramlize data')
-sc.pp.normalize_total(adata_rna)
-sc.pp.log1p(adata_rna)
-sc.pp.scale(adata_rna)
+adata_rna.X = adata_rna.layers['lognorm'] #TODO: fix this
 groups = adata_rna.obs.cell_type
 gene_names = adata_rna.var.gene_ids.index.to_numpy()
 X = adata_rna.X
@@ -40,22 +35,21 @@ X = adata_rna.X
 tfs = np.loadtxt(par["tf_all"], dtype=str)
 tf_names = [gene_name for gene_name in gene_names if (gene_name in tfs)]
 
+# GRN inference
+client = Client(processes=False)
 
+def infer_grn(X, par):
+  print("Infer grn", flush=True)
+  
+  network = grnboost2(X, client_or_address=client, gene_names=gene_names, tf_names=tf_names)
+  network.rename(columns={'TF': 'source', 'target': 'target', 'importance': 'weight'}, inplace=True)
+  network.reset_index(drop=True, inplace=True)
+  network = process_links(network, par)
+  
+  return network
 
 
 if par['cell_type_specific']:
-    # GRN inference
-  client = Client(processes=False)
-
-  def infer_grn(X, par):
-    print("Infer grn", flush=True)
-    
-    network = grnboost2(X, client_or_address=client, gene_names=gene_names, tf_names=tf_names)
-    network.rename(columns={'TF': 'source', 'target': 'target', 'importance': 'weight'}, inplace=True)
-    network.reset_index(drop=True, inplace=True)
-    network = process_links(network, par)
-    
-    return network
     i = 0
     for group in tqdm(np.unique(groups), desc="Processing groups"):
         X_sub = X[groups == group, :]
@@ -67,17 +61,7 @@ if par['cell_type_specific']:
             grn = pd.concat([grn, net], axis=0).reset_index(drop=True)
         i += 1
 else:
-  local_cluster = LocalCluster(n_workers=10, 
-                             threads_per_worker=1)
-
-  custom_client = Client(local_cluster)
-  network = grnboost2(X, client_or_address=custom_client, gene_names=gene_names, tf_names=tf_names)
-  network.rename(columns={'TF': 'source', 'target': 'target', 'importance': 'weight'}, inplace=True)
-  network.reset_index(drop=True, inplace=True)
-  grn = process_links(network, par)
-  custom_client.close()
-  local_cluster.close()
-           
+    grn = infer_grn(X, par)       
 
 # Save inferred GRN
 grn.to_csv(par['prediction'], sep=',')
