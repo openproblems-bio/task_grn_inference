30547589684095c9d04b944ea437e23214fe542b
jalil <jalil.nourisa@gmail.com>
2024-09-18 08:36:52 +0200

    small changes to scenic+

--- src/methods/multi_omics/scenicplus/test.sh
+++ src/methods/multi_omics/scenicplus/test.sh
@@ -1,2 +1,7 @@
-#viash run src/methods/multi_omics/scenicplus/config.vsh.yaml -p docker -- ---setup build
-viash run src/methods/multi_omics/scenicplus/config.vsh.yaml -- --multiomics_atac resources_test/grn-benchmark/multiomics_atac.h5ad --multiomics_rna resources_test/grn-benchmark/multiomics_rna.h5ad --temp_dir output/scenicplus --prediction output/scenicplus/prediction.csv --cell_topic output/scenicplus/cell_topic.csv --scplus_mdata output/scenicplus/scplus_mdata.h5mu
+viash run src/methods/multi_omics/scenicplus/config.vsh.yaml -- --multiomics_atac ./resources_test/grn-benchmark/multiomics_atac.h5ad \
+    --multiomics_rna ./resources_test/grn-benchmark/multiomics_rna.h5ad \
+    --tf_all ./resources/prior/tf_all.csv  \
+    --prediction ./output/scenicplus/prediction.csv \
+    --cell_topic ./output/scenicplus/cell_topic.csv \
+    --scplus_mdata ./output/scenicplus/scplus_mdata.h5mu \
+    --temp_dir ./output/scenicplus/
