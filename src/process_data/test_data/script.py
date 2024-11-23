94c79b7c8eaeaa13bd9a9b797307fb0362b3b7d2
Jalil Nourisa <jalil.nourisa@gmail.com>
2024-11-23 21:43:55 +0100

    bug in api fixed

--- src/process_data/test_data/script.py
+++ src/process_data/test_data/script.py
@@ -21,11 +21,11 @@ par = {
     'perturbation_data': 'resources/evaluation_datasets/op_perturbation.h5ad',
     'perturbation_data_test': 'resources_test/evaluation_datasets/op_perturbation.h5ad',
 
-    'multiomics_counts': 'resources/datasets_raw/op_multiome_counts.h5ad',
-    'multiomics_counts_test': 'resources_test/datasets_raw/op_multiome_counts.h5ad',
+    'multiomics_counts': 'resources/datasets_raw/op_multiome_sc_counts.h5ad',
+    'multiomics_counts_test': 'resources_test/datasets_raw/op_multiome_sc_counts.h5ad',
 
-    'perturbation_counts': 'resources/datasets_raw/op_perturbation_counts.h5ad',
-    'perturbation_counts_test': 'resources_test/datasets_raw/op_perturbation_counts.h5ad',
+    'perturbation_counts': 'resources/datasets_raw/op_perturbation_sc_counts.h5ad',
+    'perturbation_counts_test': 'resources_test/datasets_raw/op_perturbation_sc_counts.h5ad',
 }
 ## VIASH END
 
