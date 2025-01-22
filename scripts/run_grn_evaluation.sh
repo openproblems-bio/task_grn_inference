e1fa5a1553473c325e65c8c73a3bae0d5829b8c3
Jalil Nourisa <jalil.nourisa@gmail.com>
2025-01-22 13:18:26 +0100

    change to run evaluation

--- scripts/run_grn_evaluation.sh
+++ scripts/run_grn_evaluation.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-RUN_ID="d0_hvgs"
+RUN_ID="scores_workflow"
 echo $RUN_ID
-resources_dir="s3://openproblems-data/resources/grn"
-# resources_dir="./resources"
+# resources_dir="s3://openproblems-data/resources/grn/"
+resources_dir="./resources/"
 publish_dir="${resources_dir}/results/${RUN_ID}"
-grn_models_folder="${resources_dir}/grn_models/d0_hvgs"
+
+dataset="norman"
+grn_models_folder="${resources_dir}/grn_models/" #TODO: change this
 
 reg_type="ridge"
-subsample=-2
 num_workers=10
-layer=scgen_pearson
 metric_ids="[regression_1, regression_2]"
 
 param_file="./params/${RUN_ID}.yaml"
@@ -31,21 +31,21 @@ param_file="./params/${RUN_ID}.yaml"
 #     "pearson"
 #     )
 grn_names=(
-    "scglue"
-    "celloracle"
+    # "scglue"
+    # "celloracle"
 
     "grnboost2"
-    "genie3"
-    "ppcor"
-    "scenic"
-    "portia"
+    # "genie3"
+    # "ppcor"
+    # "scenic"
+    # "portia"
     
-    "negative_control"
-    "positive_control"
-    "pearson_causal"
+    # "negative_control"
+    # "positive_control"
+    # "pearson_causal"
     "pearson_corr"
 
-    "collectri"
+    # "collectri"
     )
 echo $grn_names
 # Start writing to the YAML file
@@ -57,15 +57,14 @@ append_entry() {
   cat >> $param_file << HERE
   - id: ${reg_type}_${1}
     metric_ids: ${metric_ids}
-    perturbation_data: ${resources_dir}/grn-benchmark/perturbation_data.h5ad
+    evaluation_data: ${resources_dir}/evaluation_datasets/${dataset}_perturbation.h5ad
+    evaluation_data_sc: ${resources_dir}/evaluation_datasets/${dataset}_sc_counts.h5ad
     reg_type: $reg_type
     method_id: $1
-    subsample: $subsample
     num_workers: $num_workers
     tf_all: ${resources_dir}/prior/tf_all.csv
-    layer: ${layer}
-    consensus: ${resources_dir}/prior/consensus-num-regulators.json
-    prediction: ${grn_models_folder}/$1.csv
+    regulators_consensus: ${resources_dir}/prior/regulators_consensus_${dataset}.json
+    prediction: ${grn_models_folder}/${dataset}/$1.csv
 HERE
 }
 
@@ -80,13 +79,13 @@ output_state: "state.yaml"
 publish_dir: "$publish_dir"
 HERE
 
-# nextflow run . \
-#   -main-script  target/nextflow/workflows/run_grn_evaluation/main.nf \
-#   -profile docker \
-#   -with-trace \
-#   -c src/common/nextflow_helpers/labels_ci.config \
-#   -params-file ${param_file}
-# subl resources/results/grn_evaluation_all_ridge/scores.yaml
+nextflow run . \
+  -main-script  target/nextflow/workflows/run_grn_evaluation/main.nf \
+  -profile docker \
+  -with-trace \
+  -c src/common/nextflow_helpers/labels_ci.config \
+  -params-file ${param_file}
+
 
 # ./tw-windows-x86_64.exe launch `
 #     https://github.com/openproblems-bio/task_grn_inference.git `
@@ -99,11 +98,11 @@ HERE
 #     --config src/common/nextflow_helpers/labels_tw.config
 
 
-./tw launch https://github.com/openproblems-bio/task_grn_inference \
-  --revision build/main \
-  --pull-latest \
-  --main-script target/nextflow/workflows/run_grn_evaluation/main.nf \
-  --workspace 53907369739130 \
-  --compute-env 6TeIFgV5OY4pJCk8I0bfOh \
-  --params-file ${param_file} \
-  --config src/common/nextflow_helpers/labels_tw.config
+# ./tw launch https://github.com/openproblems-bio/task_grn_inference \
+#   --revision build/main \
+#   --pull-latest \
+#   --main-script target/nextflow/workflows/run_grn_evaluation/main.nf \
+#   --workspace 53907369739130 \
+#   --compute-env 6TeIFgV5OY4pJCk8I0bfOh \
+#   --params-file ${param_file} \
+#   --config src/common/nextflow_helpers/labels_tw.config
