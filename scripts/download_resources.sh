402ed76134aec458ed89b17db7b835f7776a10c3
Robrecht Cannoodt <rcannood@gmail.com>
2025-01-30 10:36:50 +0100

    update some scripts

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -2,26 +2,4 @@
 
 set -e
 
-echo ">> Downloading resources"
-
-viash run src/common/sync_test_resources/config.vsh.yaml -- \
-  --input "s3://openproblems-data/resources/grn/inference_datasets/" \
-  --output "resources/inference_datasets/" \
-  --delete
-
-viash run src/common/sync_test_resources/config.vsh.yaml -- \
-  --input "s3://openproblems-data/resources/grn/evaluation_datasets/" \
-  --output "resources/evaluation_datasets/" \
-  --delete
-
-viash run src/common/sync_test_resources/config.vsh.yaml -- \
-  --input "s3://openproblems-data/resources/grn/prior" \
-  --output "resources/prior" \
-  --delete
-
-viash run src/common/sync_test_resources/config.vsh.yaml -- \
-  --input "s3://openproblems-data/resources/grn/grn_models/" \
-  --output "resources/grn_models/" \
-  --delete
-
-
+common/scripts/sync_resources
