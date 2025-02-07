aa9c057075992cecbcf6abe330d9608be1f89504
Jalil Nourisa <jalil.nourisa@gmail.com>
2025-02-07 12:02:34 +0100

    metrics updated

--- src/methods/single_omics/grnboost2/test.sh
+++ src/methods/single_omics/grnboost2/test.sh
@@ -1,4 +1,6 @@
-viash run src/methods/single_omics/grnboost2/config.vsh.yaml -- --multiomics_rna resources_test/grn-benchmark/multiomics_rna.h5ad \
+
+
+viash run src/methods/single_omics/grnboost2/config.vsh.yaml -- \
+    --rna resources_test/grn_benchmark/inference_data/op_rna.h5ad \
     --tf_all resources/grn_benchmark/prior/tf_all.csv \
-    --cell_type_specific false \
-    --prediction output/grnboost2/prediction.csv
+    --prediction output/grnboost2.h5ad
