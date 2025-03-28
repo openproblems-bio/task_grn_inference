49a9011ea716d8012e8e60f5b402b6c8c3ac8f23
jalil nourisa <jalil.nourisa@gmail.com>
2025-03-29 00:57:38 +0100

    a lighter docker image for scenicplus

--- src/methods/single_omics/portia/test.sh
+++
@@ -1,4 +0,0 @@
-viash run src/methods/single_omics/portia/config.vsh.yaml -- --multiomics_rna resources_test/grn-benchmark/multiomics_rna.h5ad \
-    --tf_all resources/grn_benchmark/prior/tf_all.csv \
-    --prediction output/portia/prediction.csv \
-    --cell_type_specific false
