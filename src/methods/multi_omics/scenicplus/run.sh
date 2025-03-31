a23a4f3b5e1a2e1be9238cb81678eea318fc0fc9
jalil nourisa <jalil.nourisa@gmail.com>
2025-03-31 13:34:11 +0200

    run_umap was removed from sp

--- src/methods/multi_omics/scenicplus/run.sh
+++ src/methods/multi_omics/scenicplus/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=20
 #SBATCH --time=48:00:00
 #SBATCH --mem=500GB
 #SBATCH --partition=gpu
@@ -11,11 +11,12 @@
 #SBATCH --mail-type=END,FAIL      
 #SBATCH --mail-user=jalil.nourisa@gmail.com
 
-# singularity run ../../images/scenicplus python src/methods/multi_omics/scenicplus/script.py 
-viash run src/methods/multi_omics/scenicplus/config.vsh.yaml -- \
-    --rna resources_test/grn_benchmark/inference_data/op_rna.h5ad \
-    --atac resources_test/grn_benchmark/inference_data/op_atac.h5ad \
-    --tf_all resources_test/grn_benchmark/prior/tf_all.csv \
-    --prediction output/sp_test/prediction.h5ad \
-    --temp_dir output/sp_test/ \
-    --num_workers 10
+# viash run src/methods/multi_omics/scenicplus/config.vsh.yaml -- \
+#     --rna resources_test/grn_benchmark/inference_data/op_rna.h5ad \
+#     --atac resources_test/grn_benchmark/inference_data/op_atac.h5ad \
+#     --tf_all resources_test/grn_benchmark/prior/tf_all.csv \
+#     --prediction output/sp_test/prediction.h5ad \
+#     --temp_dir output/sp_test/ \
+#     --num_workers 10
+
+singularity run ../../images/scenicplus python src/methods/multi_omics/scenicplus/script.py
