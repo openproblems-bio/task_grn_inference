519fe854b990b514940fa2932393cc244c552c71
jkobject <jkobject@gmail.com>
2025-04-29 10:33:11 +0200

    Revert "nothing really"
    This reverts commit 0b793ad218488dd661254cd854d7b57e945afb6c.

--- src/methods/single_omics/grnboost2/script.py
+++ src/methods/single_omics/grnboost2/script.py
@@ -9,26 +9,27 @@ import argparse
 
 ## VIASH START
 par = {
-    "rna": "resources/grn-benchmark/rna_d0_hvg.h5ad",
-    "tf_all": "resources/grn_benchmark/prior/tf_all.csv",
-    "prediction": "output/grnboost2_donor_0_hvg.csv",
-    "max_n_links": 50000,
-    "cell_type_specific": False,
-    "normalize": False,
-    "qc": False,
-    "layer": "X_norm",
+  'rna': 'resources/grn-benchmark/rna_d0_hvg.h5ad',
+  "tf_all": 'resources/grn_benchmark/prior/tf_all.csv',
+  'prediction': 'output/grnboost2_donor_0_hvg.csv',
+  'max_n_links': 50000,
+  'cell_type_specific': False,
+  'normalize': False,
+  'qc': False,
+  'layer': 'X_norm'
 }
 ## VIASH END
 
 ## LOCAL START
 parser = argparse.ArgumentParser(description="Process multiomics RNA data.")
-parser.add_argument("--rna", type=str, help="Path to the multiomics RNA file")
-parser.add_argument("--prediction", type=str, help="Path to the prediction file")
-parser.add_argument("--resources_dir", type=str, help="Path to the prediction file")
-parser.add_argument("--tf_all", type=str, help="Path to the tf_all")
-parser.add_argument("--max_n_links", type=str, help="Number of top links to retain")
-parser.add_argument("--dataset_id", type=str, help="Dataset id")
-parser.add_argument("--normalize", action="store_true")
+parser.add_argument('--rna', type=str, help='Path to the multiomics RNA file')
+parser.add_argument('--prediction', type=str, help='Path to the prediction file')
+parser.add_argument('--resources_dir', type=str, help='Path to the prediction file')
+parser.add_argument('--tf_all', type=str, help='Path to the tf_all')
+parser.add_argument('--num_workers', type=str, help='Number of cores')
+parser.add_argument('--max_n_links', type=str, help='Number of top links to retain')
+parser.add_argument('--dataset_id', type=str, help='Dataset id')
+parser.add_argument('--normalize', action='store_true')
 args = parser.parse_args()
 
 par_local = vars(args)
@@ -42,27 +43,28 @@ for key, value in par_local.items():
 try:
     sys.path.append(meta["resources_dir"])
 except:
-    meta = {"resources_dir": "src/utils"}
+    meta = {
+    'resources_dir': 'src/utils'
+    }
     sys.path.append(meta["resources_dir"])
 
 from util import process_links, basic_qc
 
-
 def main(par: dict) -> pd.DataFrame:
-    """
-    Main function to infer GRN
-    """
-    print("Reading data")
-    adata_rna = ad.read_h5ad(par["rna"])
-    if "qc" in par:
-        if par["qc"]:
-            print("Shape before QC: ", adata_rna.shape)
+    '''
+        Main function to infer GRN
+    '''
+    print('Reading data')
+    adata_rna = ad.read_h5ad(par['rna'])
+    if 'qc' in par:
+        if par['qc']:
+            print('Shape before QC: ', adata_rna.shape)
             adata_rna = basic_qc(adata_rna)
-            print("Shape after QC: ", adata_rna.shape)
+            print('Shape after QC: ', adata_rna.shape)
 
     gene_names = adata_rna.var_names
 
-    X = adata_rna.layers[par["layer"]]
+    X = adata_rna.layers[par['layer']] 
 
     # Load list of putative TFs
     tfs = np.loadtxt(par["tf_all"], dtype=str)
@@ -72,34 +74,21 @@ def main(par: dict) -> pd.DataFrame:
     client = Client(processes=False)
 
     print("Infer grn", flush=True)
-
-    network = grnboost2(
-        X, client_or_address=client, gene_names=gene_names, tf_names=tf_names
-    )
-    network.rename(
-        columns={"TF": "source", "target": "target", "importance": "weight"},
-        inplace=True,
-    )
+  
+    network = grnboost2(X, client_or_address=client, gene_names=gene_names, tf_names=tf_names)
+    network.rename(columns={'TF': 'source', 'target': 'target', 'importance': 'weight'}, inplace=True)
     network.reset_index(drop=True, inplace=True)
-    network = process_links(network, par)
-    return network
+    network = process_links(network, par)  
+    return network  
 
+if __name__ == '__main__':
+    dataset_id = ad.read_h5ad(par['rna'], backed='r').uns['dataset_id']
 
-if __name__ == "__main__":
-    dataset_id = ad.read_h5ad(par["rna"], backed="r").uns["dataset_id"]
-
-    net = main(par)
+    net = main(par)   
 
     # Save inferred GRN
-    print("Output GRN")
+    print('Output GRN')
     # convert the predictions to the benchmark format
     net = net.astype(str)
-    output = ad.AnnData(
-        X=None,
-        uns={
-            "method_id": "grnboost2",
-            "dataset_id": dataset_id,
-            "prediction": net[["source", "target", "weight"]],
-        },
-    )
-    output.write(par["prediction"])
+    output = ad.AnnData(X=None, uns={"method_id": 'grnboost2', "dataset_id": dataset_id, "prediction": net[["source", "target", "weight"]]})
+    output.write(par['prediction'])
